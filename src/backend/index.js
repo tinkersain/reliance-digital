4788ecf06452a72b91fd00a332a9629419cacb9d
tinkersain <tanishakar04@gmail.com>
2024-07-28 15:42:29 +0530

    local changes

--- src/backend/index.js
+++ src/backend/index.js
@@ -3,9 +3,10 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
+const { mongoose } = require("mongoose");
 const PORT = process.env.PORT || 3000;
 const PUBLIC_URL = process.env.PUBLIC_URL;
-console.log(PUBLIC_URL);
+const MONGO_URL = process.env.MONGO_URL;
 
 const app = express();
 app.use(express.json());
@@ -20,11 +21,13 @@ app.use(
 app.options("*", cors());
 
 //Define the route
-app.post("/getname", async (req, res) => {
-  res.status(200).json("Tanisha");
+app.get("/", async (req, res) => {
+  await mongoose.connect(MONGO_URL);
+  console.log("connected");
+  res.status(200).json("Hello");
 });
 
 //Start the server
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
 });
